684aa8dc52c7d16843e89d5ae8e230409413b160
JustEllis <68213771+JustEllis@users.noreply.github.com>
2024-03-24 19:20:49 +1000

    Initial Start
    Starting pieces

---
+++ Assignment1/src/assignment1/BookTest.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+public class BookTest {
+    public static void main(String[] args) {
+        BookDataFile bookDataFile = new BookDataFile();
+
+        // Creating two book objects
+        Member book1 = new Member("Network Security", "Mark Ciampa", "Thomson Course Technology", 2005, "0-619-21566-6");
+        Member book2 = new Member("Firewalls and Network Security", "Greg Holden", "Thomson Course Technology", 2005,
+                "0-619-13039-3");
+
+        // Saving books to the file
+        bookDataFile.saveBook(book1);
+        bookDataFile.saveBook(book2);
+
+        // Reading back and displaying books from the file
+        bookDataFile.displayBooks();
+    }
+}
---
+++ Assignment1/src/assignment1/Member.java
@@ -0,0 +1,69 @@
+package assignment1;
+import java.io.Serializable;
+
+/**
+ *
+ * @author wiekiang
+ */
+
+public class Member implements Serializable {
+    private int memberN;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int phoneN;
+    
+
+    // Constructor
+    public Member(int memberN, String firstName, String lastName, String address, int phoneN) {
+        this.memberN = memberN;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneN = phoneN;
+    }
+
+    // Getter and setter methods for each field
+    public int getMemberN() {
+        return memberN;
+    }
+
+    public void setMemberN(int memberN) {
+        this.memberN = memberN;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneN() {
+        return phoneN;
+    }
+
+    public void setPhoneN(int phoneN) {
+        this.phoneN = phoneN;
+    }
+
+    
+}
+  

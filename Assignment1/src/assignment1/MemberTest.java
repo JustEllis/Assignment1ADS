c5fc007c3af89c1ba9a677baf2cd9b7ce70bca2f
JustEllis <68213771+JustEllis@users.noreply.github.com>
2024-03-24 19:50:46 +1000

    attempting to commit via netbeans

--- Assignment1/src/assignment1/MemberTest.java
+++ Assignment1/src/assignment1/MemberTest.java
@@ -2,18 +2,18 @@ package assignment1;
 
 public class MemberTest {
     public static void main(String[] args) {
-        BookDataFile bookDataFile = new BookDataFile();
+        MemberDataFile memberDataFile = new MemberDataFile();
 
         // Creating two book objects
-        Member book1 = new Member("Network Security", "Mark Ciampa", "Thomson Course Technology", 2005, "0-619-21566-6");
-        Member book2 = new Member("Firewalls and Network Security", "Greg Holden", "Thomson Course Technology", 2005,
+        Member member1 = new Member(1, "Ellis", "Floriani", "10 streety street", 0406090406);
+        Member member2 = new Member("Firewalls and Network Security", "Greg Holden", "Thomson Course Technology", 2005,
                 "0-619-13039-3");
 
         // Saving books to the file
-        bookDataFile.saveBook(book1);
-        bookDataFile.saveBook(book2);
+        memberDataFile.saveMember(member1);
+        memberDataFile.saveMember(member2);
 
         // Reading back and displaying books from the file
-        bookDataFile.displayBooks();
+        memberDataFile.displayMembers();
     }
 }

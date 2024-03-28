3a6c9eed3751c590a0a6dcf8147031279b36e406
JustEllis <68213771+JustEllis@users.noreply.github.com>
2024-03-28 23:32:29 +1000

    Objects
    Managed to get my assignment to create objects but am still struggling on how to transfer the txt data over into the object.

--- Assignment1/src/assignment1/TCPServer.java
+++ Assignment1/src/assignment1/TCPServer.java
@@ -4,13 +4,24 @@ import java.net.*;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TCPServer {
+
 	public static void main (String args[]) {
+                
 		try{
-                    
-       
                         
+                        int interval = 2000; //Print to file every 2 seconds
+
+                        java.util.Timer tm = new java.util.Timer(); // using timer from util package
+
+                        //schedule timer to write to file after interval and repeat every interval
+                        tm.schedule(new WriteToFile(), interval, interval);
+                    
 			int serverPort = 1101;
 			ServerSocket listenSocket = new ServerSocket(serverPort);
             //System.out.println("TCP Server running...");
@@ -22,18 +33,17 @@ public class TCPServer {
 			}
 
 		} catch(IOException e) {System.out.println("Listen socket:"+e.getMessage());}
-	}
+	
+        }
         
+              
 }
 
 class Connection extends Thread {
-
 	ObjectInputStream in;
 	ObjectOutputStream out;
 	Socket clientSocket;
-        
-        private PrintWriter prn = null;
-        
+         
 	public Connection (Socket aClientSocket) {
 
 		try {
@@ -42,14 +52,24 @@ class Connection extends Thread {
 			out =new ObjectOutputStream( clientSocket.getOutputStream());
 			this.start();
 		} catch(IOException e) {System.out.println("Connection:"+e.getMessage());}
-	}
-
+	
+        
+       
+        }
+        
 	public void run(){
                 MemberDataFile memberDataFile = new MemberDataFile();
-                
+                String filename = "memberListObject";
+                FileOutputStream fos = null;
 		try {
                         
-			Member member = (Member)in.readObject();	
+			Member member = (Member)in.readObject();
+                       
+                        fos = new FileOutputStream(filename);
+                        out = new ObjectOutputStream(fos); 
+                           
+                        
+                        
 			/*
 			System.out.println("The Recevied Gym Member Data:");
 			System.out.println("====================================");
@@ -66,36 +86,58 @@ class Connection extends Thread {
                        //String data = in.readUTF();
                         // System.out.println(data);
                         //out.writeUTF("Info has been saved");
-                        
-                        
-                prn = new PrintWriter(new FileOutputStream(new File("memberList.txt"), true /* append = true */));
-                //Read system date
-                Date date = new Date();
-                //Date formatter
-                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-				//foramtt date to the given mask
-				String dateString=formatter.format(date);
-				// Display on the screen
-                System.out.println( "Current time of the day using Date - 12 hour format: " + dateString);
-                //Write the same contents to the file
-                prn.println("Current time of the day using Date - 12 hour format: " + dateString);
-                //close the file
-                prn.close();
-                                               
-
+                        out.writeObject(member);  
+                                            
 		}catch (EOFException e){System.out.println("EOF:"+e.getMessage());
 		} catch(IOException e) {System.out.println("readline:"+e.getMessage());
 		} catch(ClassNotFoundException ex){
 					 ex.printStackTrace();
 		}finally{ try {clientSocket.close();}catch (IOException e){/*close failed*/}}
 
-	}
+            }
+           
+        	
+    }
+
+
+class WriteToFile extends TimerTask implements Serializable{
+
+            //this method is called automatically when the task is scheduled
+            public void run() {
+                
+               
+                Member member = null;
+                
+                String filename1 = "memberList.txt";
+                FileInputStream fis = null;
+                ObjectInputStream in = null;
+                
+                 String filename2= "memberListObject";
+                FileOutputStream fos = null;
+                ObjectOutputStream out = null;
+                
+                
+                try{
+                fis = new FileInputStream(filename1);
+                //in = new ObjectInputStream(fis);
+                member = (Member)fis.readObject();
+                fis.close();
+                
+                
+                fos = new FileOutputStream(filename2);
+                out = new ObjectOutputStream(fos);
+                out.writeObject(member);
+                out.close();
+                
+                
+                System.out.println("Test");
+             
+                }//end of try block
+                catch(IOException ex) //exception handling for file handling
+                {
+                  ex.printStackTrace();
+                }  catch(ClassNotFoundException ex){
+					 ex.printStackTrace();
+            }
+    }
 }
-
-			
-
-
-
-
-
-
